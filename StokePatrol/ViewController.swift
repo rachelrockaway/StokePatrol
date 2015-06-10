364d2693d49b99af1695a816acd81fd5deccaf75
Rachel <rachel@rachelrockaway.com>
2015-06-10 09:40:59 -0400

    Test Parse SDK.

--- StokePatrol/ViewController.swift
+++ StokePatrol/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015 rachelrockaway. All rights reserved.
 //
 
+import Parse
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
