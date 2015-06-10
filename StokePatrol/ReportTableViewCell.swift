54ee3f060a11deb0adc33dc9752147882722da73
Rachel <rachel@rachelrockaway.com>
2015-06-10 14:03:17 -0400

    Create timeline table view controller, edit report table view controller, and report table view cell files and add Parse SDK imports.

---
+++ StokePatrol/ReportTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ReportTableViewCell.swift
+//  StokePatrol
+//
+//  Created by Rachel Stevenson on 6/10/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import Parse
+import UIKit
+
+class ReportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
