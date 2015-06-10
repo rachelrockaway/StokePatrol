097a3bf13085ef8fa669ea827ef8a6eb0df5780f
Rachel <rachel@rachelrockaway.com>
2015-06-10 14:14:35 -0400

    Add outlets for report table view cell labels.

--- StokePatrol/ReportTableViewCell.swift
+++ StokePatrol/ReportTableViewCell.swift
@@ -11,6 +11,33 @@ import UIKit
 
 class ReportTableViewCell: UITableViewCell {
 
+    
+    //Data Variables
+    
+    //Outlets
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    @IBOutlet weak var reportTextView: UITextView!
+    
+    
+    @IBOutlet weak var waveHeightLabel: UILabel!
+    
+    
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    
+    
+    @IBOutlet weak var tideDirectionLabel: UILabel!
+    
+    
+    
+    //Actions
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
