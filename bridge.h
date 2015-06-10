4811260d8e4b3ae23c3dd8f80363b35b68d7877c
Rachel <rachel@rachelrockaway.com>
2015-06-10 09:18:29 -0400

    Initial commit. Add bridging header.

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  StokePatrol
+//
+//  Created by Rachel Stevenson on 6/10/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+#ifndef StokePatrol_bridge_h
+#define StokePatrol_bridge_h
+
+
+#endif
